7029f045b5fdd066f3f53f4d4f0defb103bab686
Dariusz Szlag <darek.szlag@gmail.com>
2022-06-23 22:01:44 +0200

    Removed API key

--- app/src/main/java/com/dariusz/compactweather/utils/Constants.kt
+++ app/src/main/java/com/dariusz/compactweather/utils/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
 
     //API SETUP
     const val API_URL = "https://dataservice.accuweather.com/"
-    const val API_KEY = "fj7C6S1DCVtYvyWBSIGKrBCvBELCc3Tj"
+    const val API_KEY = "" //obtain your own API key for free at https://developer.accuweather.com
 
     //DB SETUP
     const val DB_NAME = "compact_weather_db"
